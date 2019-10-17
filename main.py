e95654f11ffd353c9bb11a2e855e4af6a66a8d7d
UtopiaMint <admin@lkp111138.me>
2019-10-17 14:35:23 +0800

    Added mod commands

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 import config
 import logging
-from telegram import Update, Message, Bot
+from functools import wraps
+from telegram import Update, Message, Bot, ChatPermissions
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, CallbackContext)
 
 logging.basicConfig(level=logging.INFO,
@@ -10,36 +11,90 @@ logging.basicConfig(level=logging.INFO,
 logger = logging.getLogger(__name__)
 
 
-def main():
-    updater = Updater(config.BOT_TOKEN, use_context=True)
-    updater.dispatcher.add_handler(CommandHandler('start', cmd_start))
-    updater.dispatcher.add_handler(CommandHandler('id', cmd_id))
-    updater.dispatcher.add_handler()
+def restricted(func):
+    @wraps(func)
+    def wrapped(update, context, *args, **kwargs):
+        user_id = update.effective_user.id
+        if user_id != config.OWNER_ID:
+            logger.log(logging.INFO, "Unauthorized access to /{} denied for {}.".format(func.__name__, user_id))
+            return
+        return func(update, context, *args, **kwargs)
+    return wrapped
 
-    updater.start_polling()
-    updater.idle()
+
+def reply(message: Message, bot: Bot, text):
+    bot.send_message(message.chat_id, text, reply_to_message_id=message.message_id, parse_mode="html")
 
 
 def cmd_id(update: Update, context: CallbackContext):
     # if not a reply, display both group and user id
     message: Message = update.message
     if message.reply_to_message:
-        reply(update.message, context.bot, f'Sender ID: <code>{message.reply_to_message.from_user.id}</code>')
+        reply(message, context.bot, f'Sender ID: <code>{message.reply_to_message.from_user.id}</code>')
     else:
-        reply(update.message, context.bot, f'Your ID: <code>{message.from_user.id}</code>\n'
+        reply(message, context.bot, f'Your ID: <code>{message.from_user.id}</code>\n'
               f'Group ID: <code>{message.chat_id}</code>')
     pass
 
 
 def cmd_start(update: Update, context: CallbackContext):
-    print(update.message)
-    print(context.chat_data)
     update.message.reply_text('start!', quote=True)
-    pass
 
 
-def reply(message: Message, bot: Bot, text):
-    bot.send_message(message.chat_id, text, reply_to_message_id=message.message_id, parse_mode="html")
+# mod commands
+@restricted
+def cmd_ban(update: Update, context: CallbackContext):
+    if update.message.reply_to_message:
+        replied = update.message.reply_to_message
+        context.bot.kick_chat_member(replied.chat_id, replied.from_user.id)
+        reply(update.message, context.bot, 'User banned!')
+
+
+@restricted
+def cmd_unban(update: Update, context: CallbackContext):
+    if update.message.reply_to_message:
+        replied = update.message.reply_to_message
+        context.bot.unban_chat_member(replied.chat_id, replied.from_user.id)
+
+
+@restricted
+def cmd_kick(update: Update, context: CallbackContext):
+    if update.message.reply_to_message:
+        replied = update.message.reply_to_message
+        context.bot.kick_chat_member(replied.chat_id, replied.from_user.id)
+        context.bot.unban_chat_member(replied.chat_id, replied.from_user.id)
+        reply(update.message, context.bot, 'User kicked!')
+
+
+@restricted
+def cmd_mute(update: Update, context: CallbackContext):
+    if update.message.reply_to_message:
+        replied = update.message.reply_to_message
+        context.bot.restrict_chat_member(replied.chat_id, replied.from_user.id, ChatPermissions())
+        reply(update.message, context.bot, 'User muted!')
+
+
+@restricted
+def cmd_unmute(update: Update, context: CallbackContext):
+    if update.message.reply_to_message:
+        replied = update.message.reply_to_message
+        context.bot.restrict_chat_member(replied.chat_id, replied.from_user.id,
+                                         ChatPermissions(True, True, True, True, True, True, True, True))
+
+
+def main():
+    updater = Updater(config.BOT_TOKEN, use_context=True)
+    updater.dispatcher.add_handler(CommandHandler('start', cmd_start))
+    updater.dispatcher.add_handler(CommandHandler('id', cmd_id))
+    updater.dispatcher.add_handler(CommandHandler('ban', cmd_ban))
+    updater.dispatcher.add_handler(CommandHandler('unban', cmd_unban))
+    updater.dispatcher.add_handler(CommandHandler('kick', cmd_kick))
+    updater.dispatcher.add_handler(CommandHandler('mute', cmd_mute))
+    updater.dispatcher.add_handler(CommandHandler('unmute', cmd_unmute))
+    # updater.dispatcher.add_handler()
+
+    updater.start_polling()
+    updater.idle()
 
 
 if __name__ == '__main__':
