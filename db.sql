0d0dc39c70c9e02f09f12aafefe184b2acde9168
UtopiaMint <admin@lkp111138.me>
2019-10-18 17:05:05 +0800

    Updated db schema

--- db.sql
+++ db.sql
@@ -1,8 +1,17 @@
-CREATE TABLE `bulletin` (
+CREATE TABLE `BULLETIN` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `gid` bigint(20) NOT NULL,
-  `content` text NOT NULL COLLATE utf8mb4_bin,
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
   `expires` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
+  `msg_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`msg_id`,`id`),
+  UNIQUE KEY `BULLETIN_id_uindex` (`id`),
   KEY `bulletin_gid_index` (`gid`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `group_settings` (
+  `gid` bigint(20) NOT NULL,
+  `welcome` text,
+  `flood_threshold` int(11) DEFAULT NULL,
+  PRIMARY KEY (`gid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
