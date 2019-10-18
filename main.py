005028faae60c145a05ef53c7c97bccd2f18fb9f
UtopiaMint <admin@lkp111138.me>
2019-10-18 20:00:10 +0800

    Auto responder

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ db_conn = mysql.connector.connect(
     charset='utf8mb4'
 )
 db_cursor = db_conn.cursor()
+auto_responders = {}  # dict of dict {gid: {trigger1: response1, ...}, ...}
 
 
 def get_admin_ids(bot, chat_id):
@@ -76,6 +77,33 @@ def on_member_join(update: Update, context: CallbackContext):
                     .replace('{{groupName}}', chat.title or "")\
                     .replace('{{uid}}', str(user.id or 0))
                 reply(update.message, context.bot, msg)
+        raise DispatcherHandlerStop
+
+
+def on_message(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    message = update.effective_message
+    msg = message.text
+    if msg is None or len(msg) == 0:
+        return
+    msg = msg.lower()
+    response = None
+    try:
+        response = auto_responders[chat_id][msg]
+    except KeyError:
+        return
+    if response is not None:
+        msg_type = response['msg_type']
+        msg_text = response['msg_text']
+        if msg_type == 'text':
+            reply(update.message, context.bot, msg_text)
+        elif msg_type == 'photo':
+            context.bot.send_photo(chat_id, msg_text, reply_to_message_id=message.message_id)
+        elif msg_type == 'gif':
+            context.bot.send_animation(chat_id, msg_text, reply_to_message_id=message.message_id)
+        elif msg_type == 'voice':
+            context.bot.send_voice(chat_id, msg_text, reply_to_message_id=message.message_id)
+
         raise DispatcherHandlerStop
 
 
@@ -143,6 +171,10 @@ def cmd_bulletin(update: Update, context: CallbackContext):
         reply(update.message, context.bot, bulletin, parse_mode="markdown")
 
 
+def cmd_log(update: Update, context: CallbackContext):
+    print(update.message.reply_to_message)
+
+
 # mod commands
 @restricted
 def cmd_ban(update: Update, context: CallbackContext):
@@ -198,7 +230,56 @@ def cmd_welcome(update: Update, context: CallbackContext):
     reply(update.message, context.bot, "Welcome message set")
 
 
+@restricted
+def cmd_respond(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    if update.message.reply_to_message:
+        trigger = " ".join(context.args).lower()
+        response = update.message.reply_to_message
+        msg_type = 'text'
+        msg_text = response.text
+        # see if its an image/gif/voice note
+        if response.voice:
+            # is voice note
+            msg_type = 'voice'
+            msg_text = response.voice.file_id
+        if response.photo:
+            # is voice note
+            msg_type = 'photo'
+            msg_text = response.photo[0].file_id
+        if response.document and response.document.mime_type == 'video/mp4':
+            # gif
+            msg_type = 'gif'
+            msg_text = response.document.file_id
+        add_response_trigger(chat_id, msg_type, msg_text, trigger)
+        db_cursor.execute("INSERT INTO auto_response (gid, msg_type, msg_text, `trigger`) VALUES (%s, %s, %s, %s)",
+                          [chat_id, msg_type, msg_text, trigger])
+        db_conn.commit()
+        reply(update.message, context.bot, "Auto responder set")
+
+
+def add_response_trigger(chat_id, msg_type, msg_text, trigger):
+    responders = {}
+    try:
+        responders = auto_responders[chat_id]
+    except KeyError:
+        pass
+    responders[trigger] = {}
+    responders[trigger]['msg_type'] = msg_type
+    responders[trigger]['msg_text'] = msg_text
+    auto_responders[chat_id] = responders
+
+
 def main():
+    # auto responders
+    db_cursor.execute("SELECT gid, msg_type, msg_text, `trigger` FROM auto_response")
+    while True:
+        row = db_cursor.fetchone()
+        if row is None:
+            break
+        add_response_trigger(*row)
+
+    # handlers
     updater = Updater(config.BOT_TOKEN, use_context=True)
     updater.dispatcher.add_handler(CommandHandler('start', cmd_start))
     updater.dispatcher.add_handler(CommandHandler('id', cmd_id))
@@ -209,7 +290,10 @@ def main():
     updater.dispatcher.add_handler(CommandHandler('unmute', cmd_unmute))
     updater.dispatcher.add_handler(CommandHandler('bulletin', cmd_bulletin))
     updater.dispatcher.add_handler(CommandHandler('welcome', cmd_welcome))
+    updater.dispatcher.add_handler(CommandHandler('log', cmd_log))
+    updater.dispatcher.add_handler(CommandHandler('respond', cmd_respond))
     updater.dispatcher.add_handler(MessageHandler(Filters.status_update.new_chat_members, on_member_join))
+    updater.dispatcher.add_handler(MessageHandler(None, on_message))
 
     updater.start_polling()
     updater.idle()
